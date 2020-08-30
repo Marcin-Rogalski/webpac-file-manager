42b115584ab783b486702083c8b4bcdeb1e0ee15
Marcin Rogalski <marcin.rogalski@billennium.com>
2020-08-30 18:56:59 +0200

    Fix - added 'create file' functionality

--- src/interfaces/create-options.ts
+++ src/interfaces/create-options.ts
@@ -2,10 +2,12 @@ import ExecuteOptions from './execute-options'
 
 export interface CreateFileOptions {
     increment: boolean
+    overwrite: boolean
 }
 
 export interface CreateFolderOptions {
     increment: boolean
+    overwrite: boolean
 }
 
 export default interface CreateOptions extends ExecuteOptions, CreateFileOptions, CreateFolderOptions { }
--- src/services/create-file.ts
+++ src/services/create-file.ts
@@ -1,7 +1,20 @@
-import { existsSync, mkdirSync } from 'fs'
+import { existsSync, lstatSync, writeFileSync } from 'fs'
 import { resolve } from 'path'
 import { CreateFileOptions } from '../interfaces/create-options'
+import deleteFile from './delete-file'
 
 export default function createFile(target: string, content: string, options: CreateFileOptions): void {
     target = resolve(target)
+
+    const { increment, overwrite } = options
+
+    if (existsSync(target)) {
+        if (lstatSync(target).isDirectory()) throw new Error(`Cannot write content into a directory!\n\t${target}`)
+        if (!overwrite) throw new Error(`File already exists!\n\t${target}`)
+        if (!increment) deleteFile(target, options)
+    }
+
+    const flag = increment ? 'a' : 'w'
+
+    writeFileSync(target, content, { flag })
 }
